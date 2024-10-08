0b6d0c67e7a96bec5de577973adfd506d450c79b
James Njuguna <jamesnjugunan998@gmail.com>
2024-10-08 21:49:31 +0300

    users and todos table migration successful

---
+++ migrations/1728413190478_create-todos-table.cjs
@@ -0,0 +1,40 @@
+exports.up = async (pgm) => {
+    pgm.createTable('todos', {
+      id: {
+        type: 'serial',
+        primaryKey: true,
+        notNull: true,
+      },
+      user_id: {
+        type: 'integer',
+        notNull: true,
+        references: '"users"',  // Assuming you have a users table
+        onDelete: 'CASCADE',
+      },
+      title: {
+        type: 'varchar(255)',
+        notNull: true,
+      },
+      description: {
+        type: 'text',
+        notNull: false,
+      },
+      completed: {
+        type: 'boolean',
+        default: false,
+      },
+      created_at: {
+        type: 'timestamp',
+        default: pgm.func('current_timestamp'),
+      },
+      updated_at: {
+        type: 'timestamp',
+        default: pgm.func('current_timestamp'),
+      },
+    });
+  };
+  
+  exports.down = async (pgm) => {
+    pgm.dropTable('todos');
+  };
+  
