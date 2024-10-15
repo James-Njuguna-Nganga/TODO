c3b77cdceb79de0751c7bae76098acc358b69ab3
James Njuguna <jamesnjugunan998@gmail.com>
2024-10-15 03:09:12 +0300

    feedback

--- routes/login.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { loginController } from '../controllers/loginController.js';
-
-const login = express.Router();
-
-login.post('/', loginController);
-
-export default login;
